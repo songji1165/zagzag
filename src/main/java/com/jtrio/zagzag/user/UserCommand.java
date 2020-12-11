19bcea34851481782e13b4b62acab11c20b4e6da
songji1165 <songji1165@gmail.com>
2020-12-11 11:02:40 +0900

    validate 추가

--- src/main/java/com/jtrio/zagzag/user/UserCommand.java
+++ src/main/java/com/jtrio/zagzag/user/UserCommand.java
@@ -1,18 +1,33 @@
 package com.jtrio.zagzag.user;
 
+import com.jtrio.zagzag.enums.ErrorMsg;
 import com.jtrio.zagzag.enums.Gender;
 import com.jtrio.zagzag.model.User;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+//import static com.jtrio.zagzag.enums.ErrorMsg;
+//import static com.jtrio.zagzag.enums.ErrorMsg.EMAIL;
+
+
 public class UserCommand {
+//    private ErrorMsg errMsg;
 
     @Data
     public static class CreateUser {
+//        System.out.println(ErrorMsg.valueOf("EMAIL"));
+//        @Email(message = ErrorMsg.EMAIL.toString())
+        @Email(message = "이메일 형식을 확인해주세요.")
         private String email;
+        @NotBlank(message = "비밀번호를 입력해주세요.")
         private String pass;
+        @NotBlank(message = "이름을 입력해주세요.")
         private String name;
         private Gender gender;
         private String addr;
+
 
         public User toUser(){
             User user = new User();
