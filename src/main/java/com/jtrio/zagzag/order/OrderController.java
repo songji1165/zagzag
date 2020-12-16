4ad223c997be814a810bb8f97084b2b0ef012c3d
songji1165 <songji1165@gmail.com>
2020-12-17 00:43:29 +0900

    주문 조회 api

--- src/main/java/com/jtrio/zagzag/order/OrderController.java
+++ src/main/java/com/jtrio/zagzag/order/OrderController.java
@@ -1,16 +1,18 @@
 package com.jtrio.zagzag.order;
 
+import com.jtrio.zagzag.model.ProductOrder;
 import com.jtrio.zagzag.product.ProductCommand;
 import com.jtrio.zagzag.product.ProductDto;
 import com.jtrio.zagzag.user.UserCommand;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/order")
@@ -36,7 +38,18 @@ public class OrderController {
     @PostMapping
 //    public OrderDto order(@RequestBody UserCommand.CheckUser user, @RequestBody Long productId){
     public OrderDto order(@RequestBody OrderCommand.OrderProduct params){
-
         return orderService.order(params);
     }
+
+    @GetMapping
+    public List<OrderDto> findOrder(
+            @RequestParam(value = "userid") String userId,
+            @RequestParam(value = "startdt", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDt,
+            @RequestParam(value="enddt", required = false)
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDt
+    ){
+        return orderService.findOrder(userId, startDt, endDt);
+    }
+
 }
--- src/main/java/com/jtrio/zagzag/order/OrderRepository.java
+++ src/main/java/com/jtrio/zagzag/order/OrderRepository.java
@@ -5,7 +5,13 @@ import com.jtrio.zagzag.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<ProductOrder, Long> {
-
+    List<ProductOrder> findByCreatedBetween(LocalDateTime start, LocalDateTime end);
+    List<ProductOrder> findAll();
 }
